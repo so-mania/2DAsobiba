91870bcefbaae09d30f5fbbd3519af62a494f482
O-sawa <suppon-turtle622@ezweb.ne.jp>
2020-10-22 22:06:34 +0900

    ボールの発射テスト

---
+++ Assets/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            rb.AddForce(new Vector2(250, 1000), ForceMode2D.Impulse);
+        }
+    }
+}
